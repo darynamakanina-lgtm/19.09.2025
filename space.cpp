643fc87ff2296013e03f49054958e3cfbf211fd7
Софія <sofiia@Sofias-MacBook-Air.local>
2025-11-10 19:59:34 +0200

    start

---
+++ space.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number1 = 5;
+    int number2 = 10;
+    int sum;
+
+    sum = number1 + number2;
+
+    std::cout << "Перше число: " << number1 << std::endl;
+    std::cout << "Друге число: " << number2 << std::endl;
+    std::cout << "Сума цих чисел: " << sum << std::endl; 
+
+   
+    return 0;
+}
