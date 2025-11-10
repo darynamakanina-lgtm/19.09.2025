58b2ccceb2b573b94db6753273dc9179821ae3b1
Софія <sofiia@Sofias-MacBook-Air.local>
2025-11-10 18:10:09 +0200

    new commit

---
+++ life.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main() {
+using namespace std;
+
+    int number1 = 5;
+    int number2 = 10;
+    int sum;
+
+    sum = number1 + number2;
+
+    cout << "Перше число: " << number1 << endl;
+    cout << "Друге число: " << number2 << endl;
+    cout << "Сума цих чисел: " << sum << endl; 
+
+    return 0;
+}
