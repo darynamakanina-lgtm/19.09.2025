99592be5035bb3d39ef33d842e8ca038c33cf123
Софія <sofiia@Sofias-MacBook-Air.local>
2025-11-10 20:06:17 +0200

    branch miu

--- space.cpp
+++ space.cpp
@@ -3,15 +3,12 @@ using namespace std;
 int main() {
     int number1 = 5;
     int number2 = 10;
-    int sum;
     int difference;
 
-    sum = number1 + number2;
     difference = number1 - number2;
 
     std::cout << "Перше число: " << number1 << std::endl;
     std::cout << "Друге число: " << number2 << std::endl;
-    std::cout << "Сума цих чисел: " << sum << std::endl; 
     std::cout << "Різниця: " << difference << std::endl;
 
    
